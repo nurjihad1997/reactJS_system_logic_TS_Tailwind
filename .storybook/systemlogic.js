877333abfd1e475b8c8c2e5a6449ed6fa0bf4995
JihadFirman <43309060+nurjihad1997@users.noreply.github.com>
2022-11-11 19:46:19 +0700

    Update systemlogic.js

--- .storybook/systemlogic.js
+++ .storybook/systemlogic.js
@@ -2,7 +2,7 @@ import { create } from "@storybook/theming";
 
 export default create({
     base: 'light',
-    brandTitle: 'SYSTEMLOGIC',
+    brandTitle: 'systemlogic',
     brandUrl: 'https://systemlogic-reactjs-tailwinds.netlify.app/',
     brandImage: '/Logo.png',
     brandTarget: '_self',
