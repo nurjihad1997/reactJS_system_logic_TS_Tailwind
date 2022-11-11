c2ba262b31df30a14202e1e7850952e61c6d4b96
JihadFirman <43309060+nurjihad1997@users.noreply.github.com>
2022-11-11 19:26:17 +0700

    add theme

---
+++ .storybook/systemlogic.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+    base: 'light',
+    brandTitle: 'SYSTEMLOGIC',
+    brandUrl: 'https://systemlogic-reactjs-tailwinds.netlify.app/',
+    brandImage: '/public/Logo.png',
+    brandTarget: '_self',
+  });
