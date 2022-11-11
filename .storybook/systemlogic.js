7dec938f661a299d003d645dd783c98ea8029dc9
JihadFirman <43309060+nurjihad1997@users.noreply.github.com>
2022-11-11 19:44:15 +0700

    logo

--- .storybook/systemlogic.js
+++ .storybook/systemlogic.js
@@ -4,6 +4,6 @@ export default create({
     base: 'light',
     brandTitle: 'SYSTEMLOGIC',
     brandUrl: 'https://systemlogic-reactjs-tailwinds.netlify.app/',
-    brandImage: '/Logo1.png',
+    brandImage: '/Logo.png',
     brandTarget: '_self',
   });
